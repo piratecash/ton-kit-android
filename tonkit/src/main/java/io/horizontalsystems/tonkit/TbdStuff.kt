ac42eb9f4ae306ad7a4c5b803df58ed5575984bb
abdrasulov <abdrasulov@gmail.com>
2024-09-16 17:36:00 +0600

    Fix Address comparison

--- tonkit/src/main/java/io/horizontalsystems/tonkit/TbdStuff.kt
+++ tonkit/src/main/java/io/horizontalsystems/tonkit/TbdStuff.kt
@@ -6,6 +6,13 @@ class Address(private val addrStd: AddrStd) {
     fun toRaw() = addrStd.toString(userFriendly = false).lowercase()
     fun toUserFriendly() = addrStd.toString(userFriendly = true, bounceable = true)
 
+    override fun equals(other: Any?): Boolean {
+        if (other !is Address) return false
+        return toRaw() == other.toRaw()
+    }
+
+    override fun hashCode() = addrStd.hashCode()
+
     companion object {
         fun parse(addressStr: String): Address {
             return Address(AddrStd(addressStr))
